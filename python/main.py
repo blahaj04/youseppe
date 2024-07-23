2c0471ab4fbf534cfff0cb5109506612bf465dff
blahaj04 <juliopb2004@gmail.com>
2024-07-24 00:17:40 +0200

    primera prueba bot

--- python/main.py
+++ python/main.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
+client = commands.Bot(command_prefix = '/')
 
-client = commands.Bot()
-
-
+@client.event
+async def on_ready():
+    print("Youseppe está despierto ;3")
+    print("---------------------------")
+    
+@client.command()
+async def isAwaken(ctx):
+    await ctx.send("haiii :3")
+    
+client.run('MTI2NTQxNjQzNzQ4OTcyOTU2Nw.G651uw.2fP-EK2dqiwV0sTmXMLrwbzSKbnG7LqDJrwNXY')
