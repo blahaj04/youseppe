903d274312ae3730551ba9b0f290ada37dd15901
blahaj04 <juliopb2004@gmail.com>
2024-07-24 16:16:05 +0200

    a

--- python/main.py
+++ python/main.py
@@ -1,16 +1,16 @@
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
+from discord import app_commands
 from decouple import config
 
 client = commands.Bot(command_prefix = "/",intents=discord.Intents.all())
-slash = SlashCommand(client, sync_commands=True)
 botToken = config('YOUSEPPE_TOKEN')
 
 @client.event
 async def on_ready():
     print("Youseppe esta despierto ;3")
     print("---------------------------")
+    await client.tree.sync()
 
 @client.event
 async def on_member_join(member):
@@ -21,17 +21,15 @@ async def on_member_join(member):
 async def on_member_leave(member):
     channel = client.get_channel(1265659906996961321)
     await channel.send(f"sacaio {member.name}")
-  
-    
-@client.command()
-@slash.slash(name="haiii", description="Enviar un saludo")
-async def haiii(ctx : SlashContext):
-    await ctx.send("haiii :3")
+   
 
 
-@client.command()
-@slash.slash(name="byeee", description="Enviar una despedida")
-async def byeee(ctx: SlashContext):
-    await ctx.send("byeee :3")
+@client.event(name="haiii", description="Enviar un saludo")
+async def haiii(interaction: discord.Interaction):
+    await interaction.response.send_message("haiii :3")
+
+@client.event(name="byeee", description="Enviar una despedida")
+async def byeee(interaction: discord.Interaction):
+    await interaction.response.send_message("byeee :3")
 
 client.run(botToken)
